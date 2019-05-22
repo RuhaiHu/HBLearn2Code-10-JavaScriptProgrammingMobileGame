e44f93e188f176d4e8d6b50ad1fce9228b591631
Ruhai Hu <RuhaiHu@users.noreply.github.com>
2019-05-21 23:00:11 -0700

    2019.05.21 stream

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,46 @@
-alert("hello FAILED");
+// alert("hello FAILED");
 console.log("sub for free with amazonprime?");
 // Variables learning! WOO!!!!
 // HOW DO WE DO THAT!
 var viewers = "VIEWERS ARE AWESOME!";
+var abcdefghijklmnopqrstuvwrxyz = "alphabet";
+
+// BE CONSISTENTLY HERE... Your Awesome
+
+var consoleAlert = "Write this alert to the console for more information!"
+
+console.log(consoleAlert);
+
+var a, b, c;
+
+a = 42;
+console.log("A = " + a);
+b = 3;
+c = a + b;
+console.log("C = " + c);
+
+a += c;
+
+console.log("A = " + a);
+
+// A viewer is not an object but I might make them into one!
+
+var viewerObject = new Object();
+
+var viewerObject = {name: 'jackaltomont',
+            energy: 1,
+            lives: 2,
+            x: 1, y: 1, z: 1,
+            color: 'pink'};
+
+var arrayOfArrays = [[]];
+arrayOfArrays = [0,[0]];
+console.log(arrayOfArrays[0,[0]]);
+
+function viewerFunction(name){
+    var n = name + " is a viewer";
+    console.log(n);
+    return n;
+}
+
+console.log(viewerFunction('jackaltomont'));
